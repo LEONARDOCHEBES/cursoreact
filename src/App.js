2a87fd4a3a0338e481a97b36398b1c92a5ca664a
LEONARDOCHEBES <leonardochebes@outlook.es>
2022-01-30 11:38:11 -0300

    construccion01
    se agrego select

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'; //importa la imagen 
+import './App.css'; // importa estilo css
+
+
+/*Esta funcion lo que hace es retornar codigo html jsx y se pone los parentesis asi para
+poder escribir en varias lineas el html*/
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hola mundo <br />
-          
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
+
+      <select>
+          <option value= "meme0"> Meme 1 </option>
+          <option value= "meme1"> Meme 2 </option>
+          <option value= "meme2"> Meme 3 </option>
+          <option value= "meme3"> Meme 4 </option>
+
+      </select>
+
+
+
+
     </div>
   );
 }
