c80c7a3936c35869dfee7715c42575abbd498a97
LEONARDOCHEBES <leonardochebes@outlook.es>
2022-02-13 21:51:09 -0300

    cometnarioNosenumero
    se completaron varias cosas de la funcionalidad como la variable actualizable por funcion que salta al modificarse el input y capturando el valor de este

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import logo from './logo.svg'; //importa la imagen 
 import './App.css'; // importa estilo css
+import { useState } from 'react';
 
 
 /*Esta funcion lo que hace es retornar codigo html jsx y se pone los parentesis asi para
 poder escribir en varias lineas el html*/
 
 function App() {
+
+  /* Se crea espacio constante en memoria reservado para una variable y una funcion que
+  cuando se le envia un valor renderiza todo el dom y actualiza el valor de su variable asociada
+  en forma de array que tiene su dato ligado al span*/
+
+  const [lineaDeArriba, fncLineaDeArriba]= useState('');//captura un estado
+  const [lineaDeAbajo, fncLineaDeAbajo]= useState('');
+
+  /*funcion que salta cuando se realiza cambios en el imput y captura el value del imput y lo mete
+  en la variable para ser llamada por el span cuando se renderiza el dom */
+
+  const onChangeLineaDeArriba = function(EventoValor){
+    fncLineaDeArriba(EventoValor.target.value)
+  }
   
+  const onChangeLineaDeAbajo = function(EventoValor){
+    fncLineaDeAbajo(EventoValor.target.value)
+  }
+
   return (
     <div className="App">
 
@@ -20,16 +39,16 @@ function App() {
 
       </select><br/>
 
-        <input type="text" placeholder='Linea de arriba'/><br/>
-        <input type="text" placeholder='Linea de abajo'/><br/>   
+        <input onChange={onChangeLineaDeArriba} type="text" placeholder='Linea de arriba'/><br/>
+        <input onChange={onChangeLineaDeAbajo} type="text" placeholder='Linea de abajo'/><br/>   
 
-        <button> soy un boton </button><br/>
+        <button> Exportar </button><br/>
 
 
         <div>
-            <span>Linea de arriba</span><br/>
-            <span>Linea de abajo</span><br/>
-            <img src='/meme/'/><br/>
+            <span>{lineaDeArriba}</span><br/>
+            <span>{lineaDeAbajo}</span><br/>
+            <img src='/img/'/><br/>
         </div>
 
     </div>
